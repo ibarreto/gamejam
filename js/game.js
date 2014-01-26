86bd10dcc8c5af48fafb78d71b41cf2d62eafa7d
ibarreto <iabarreto11@gmail.com>
2014-01-25 17:47:09 -0700

    license

--- js/game.js
+++ js/game.js
@@ -23,7 +23,7 @@ var game = {
 	onload: function() {
 	
 		// init the video
-		if (!me.video.init('screen', 800, 600, true, 'auto')) {
+		if (!me.video.init('screen', 1000, 600, true, 'auto')) {
 			alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
 			return;
 		}
